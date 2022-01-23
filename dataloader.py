c1f18c96b686dcd29379529aab1e5e3e8d8caf02
wenxi <okwenxi@gmail.com>
2022-01-23 17:32:27 +0800

    rm label

--- dataloader.py
+++ dataloader.py
@@ -7,21 +7,24 @@ class EvalDataset(data.Dataset):
     def __init__(self, pred_root, label_root):
         pred_dirs = os.listdir(pred_root)
         label_dirs = os.listdir(label_root)
-        print(pred_dirs)
-        print(label_dirs)
+        # print(pred_dirs)
+        # print(label_dirs)
         dir_name_list = []
         for idir in pred_dirs:
             if idir in label_dirs:
-                pred_names = os.listdir(os.path.join(pred_root, idir))
-                label_names = os.listdir(os.path.join(label_root, idir))
-                for iname in pred_names:
-                    if iname in label_names:
-                        dir_name_list.append(os.path.join(idir, iname))
+                # pred_names = os.listdir(os.path.join(pred_root, idir))
+                # label_names = os.listdir(os.path.join(label_root, idir))
+                dir_name_list.append(idir)
+                # for iname in pred_names:
+                    # if iname in label_names:
+                        
 
         self.image_path = list(
             map(lambda x: os.path.join(pred_root, x), dir_name_list))
         self.label_path = list(
             map(lambda x: os.path.join(label_root, x), dir_name_list))
+
+        print(self.image_path)
 
     def __getitem__(self, item):
         pred = Image.open(self.image_path[item]).convert('L')
